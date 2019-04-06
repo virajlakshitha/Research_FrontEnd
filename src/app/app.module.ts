d35b530ddf1c671869c86d02d0d44b0f58338d44
viraj lakshitha <virajlakshitha39@gmail.com>
2019-04-07 00:13:20 +0530

    Added footer and Menu Bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { MenuBarComponent } from './menu-bar/menu-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    MenuBarComponent
   ],
   imports: [
     BrowserModule,
