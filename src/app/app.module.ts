994213ebc2085c3a78898eee0e0ed2534e151ea8
viraj lakshitha <virajlakshitha39@gmail.com>
2019-05-08 17:22:31 +0530

    Modified the front end of Home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { MenuBarComponent } from './menu-bar/menu-bar.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    MenuBarComponent
+    MenuBarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
