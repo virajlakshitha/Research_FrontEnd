888108332d4416caff891b67154efd93fbbd3231
viraj lakshitha <virajlakshitha39@gmail.com>
2019-05-10 13:58:00 +0530

    Modified front end

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './footer/footer.component';
 import { MenuBarComponent } from './menu-bar/menu-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent,
     MenuBarComponent
   ],
   imports: [
