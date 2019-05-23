deda0cad01d65719feef5886ff15ef0b9cb51fb7
viraj lakshitha <virajlakshitha39@gmail.com>
2019-05-23 17:42:33 +0530

    Added footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { MenuBarComponent } from './menu-bar/menu-bar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    MenuBarComponent
+    MenuBarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
