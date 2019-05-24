5636eb43d3b0f72c48cb511e63abba24750b0efe
viraj lakshitha <virajlakshitha39@gmail.com>
2019-05-25 01:12:56 +0530

    Edited interfaces

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { MenuBarComponent } from './menu-bar/menu-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    FooterComponent,
+    MenuBarComponent
   ],
   imports: [
     BrowserModule,
