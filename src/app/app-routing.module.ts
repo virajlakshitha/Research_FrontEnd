baf8f118546f36388a4929e4db83ca6dbdcb3d87
sajith priyankara <sajith1995s@gmail.com>
2019-07-31 23:50:25 +0530

    chnages in search component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -53,7 +53,7 @@ const routes: Routes = [
     component: BuildForRequirementsComponent
   },
   {
-    path: "product_details/:_id",
+    path: "product_details",
     component: MoreDetailsComponent
   },
   {
